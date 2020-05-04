8dc7e2b2a1d24ee050156a7b46aa6002219040b6
Vu Nguyen <v.vunv8@vinid.net>
2020-05-04 15:59:51 +0700

    :fire: update pressDelay
    update pressDelay

--- src/TabModal.js
+++ src/TabModal.js
@@ -28,7 +28,7 @@ class ModalTabListWrapper extends SortMethod {
                         lockAxis='y'
                         // if no pressDelay, close button cannot be triggered,
                         // because it would always treat click as dnd action
-                        pressDelay='500'>
+                        pressDelay='150'>
         {this.props.children}
       </DragTabContainer>
     );
